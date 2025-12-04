9889117e95cf8a98f5883f0edeec927532993f95
Naiya <naiya@Esmes-MacBook-Air.local>
2025-12-03 19:00:59 -0500

    Rename project to readGoods

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
     return {
-        title: "Book Logger",
+        title: "readGoods",
         description: "Log your reading journey.",
         other: {
             ...(await fetchMetadata(
@@ -18,7 +18,7 @@ export default function Page() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
                 <h1 className="text-6xl font-bold">
-                    Welcome to <a className="text-blue-600" href="/">Book Logger</a>
+                    Welcome to <a className="text-blue-600" href="/">readGoods</a>
                 </h1>
                 <p className="mt-3 text-2xl">
                     A Farcaster Frame to log your books.
