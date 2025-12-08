0d2dc17dc79bdbb4cf7502da13046f082397baa4
esme <esmemadethis@gmail.com>
2025-12-08 14:58:03 -0500

    added fc icon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/.well-known/farcaster.json',
+                destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019aff68-1006-1aa7-edd8-36e2bd93e445',
+                permanent: false,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
